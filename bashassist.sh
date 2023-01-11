d78022a3b3f75c5a1c198796bcc0722aa7e80328
damienslater <damien@dp.ink>
2023-01-11 19:46:27 +0000

    Create bashassist.sh

---
+++ bashassist.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# Set the API key from the environment variable or directly in the script
+# Replace "my key" with your actual API key
+API_KEY="my key"
+
+# Set the endpoint URL for the OpenAI API
+ENDPOINT_URL="https://api.openai.com/v1/completions"
+
+# Start a new conversation by (hack alert) deleting the old one!
+rm -f ./chat.log
+
+# Continuously ask for questions
+while true; do
+
+# Display a prompt for the user to enter their question
+printf "\033[1m@You > \033[0m"
+
+# Read the user's question
+read -r QUESTION
+
+# Write the question into the chat.log
+echo -n "human: $QUESTION " >> ./chat.log
+
+# Add some nice terminal formatting
+tput sgr0
+echo -e "\e[1m---------"
+
+# Read the chat.log, including the user's last question, ready to sent to chatGPT
+QUERY=`cat ./chat.log`
+
+# Set the JSON payload for the request
+payload="{
+    \"model\":\"text-davinci-003\",
+    \"prompt\":\"$QUERY\",
+    \"max_tokens\": 3500,
+    \"temperature\": 0"
+
+# Close the JSON payload
+payload="$payload }"
+
+# Send the request to the OpenAI API
+response=$(curl -sS -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $API_KEY" -d "$payload" "$ENDPOINT_URL" 2>&1)
+
+# Extract the response from the JSON object
+RESPONSE_MESSAGE=$(echo "$response" | jq -r '.choices[0].text')
+
+# Print the response
+echo -e "\n"
+printf "\033[1m@Assistant > \033[0m"
+echo "$RESPONSE_MESSAGE"
+
+# Write the response into an reponse.log
+echo -n "ai:$RESPONSE_MESSAGE " > ./response.log
+
+# Remove rogue new-line formatting and add the response to the chat.log
+echo -n "$(<./response.log )" | tr -d '\n' >> ~/.askassist/chat.log
+
+# More fancy terminal formatting
+echo -e "\e[1m---------"
+
+done
